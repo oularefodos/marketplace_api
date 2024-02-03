45bb3862e99e67d08c0e649668c8eaec559d9bf1
Fode Oulare <foulare@e1r4p9.1337.ma>
2024-02-03 20:12:58 +0100

    Add create prima mocker function
    - instead of creating everytime the same code, i created a function that
      going to generate the object

---
+++ server/utils/prismaMock.ts
@@ -0,0 +1,53 @@
+/**
+ * @param datas - list of data
+ * @param createdData - an exemple of createdData
+ * @param key - key of the model that you wanna create
+ * @returns a Prisma mocker for a model
+ */
+export const createPrismaMocker = (
+    datas: any[],
+    createdData,
+    key: string,
+): any => {
+    const prismaMock = {};
+    const ret = {
+        create: jest.fn(() => createdData),
+        delete: jest.fn((params) =>
+            datas.find((data) => data.id === params.where.id),
+        ),
+        findUnique: jest.fn((params) =>
+            datas.find((data) => {
+                const { where } = params;
+                const keys = Object.keys(where);
+                return keys.every((key) => data[key] === where[key]);
+            }),
+        ),
+        findMany: jest.fn((params) => {
+            if (params) {
+                return (
+                    datas.filter((data) => {
+                        const { where } = params;
+                        const keys = Object.keys(where);
+                        return keys.every((key) => data[key] === where[key]);
+                    }) || []
+                );
+            } else {
+                return datas;
+            }
+        }),
+        update: jest.fn((params) => {
+            const { where, data } = params;
+            const response = datas.find((data) => data.id === where.id);
+            if (response) {
+                return {
+                    ...response,
+                    ...data,
+                };
+            } else {
+                return null;
+            }
+        }),
+    };
+    prismaMock[key] = ret;
+    return prismaMock;
+};
