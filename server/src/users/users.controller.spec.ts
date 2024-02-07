b85cb4bd5c7bb71a552d67a08928256017bb2200
Fode Oulare <foulare@e1r3p10.1337.ma>
2024-02-07 16:34:38 +0100

    add test for create User - controller

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -1,20 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { PrismaService } from "../prisma/prisma.service";
+import { CreateUserDto } from './dto/create-user.dto';
 
-describe('UsersController', () => {
-  let controller: UsersController;
+describe("UsersController", () => {
+    let controller: UsersController;
+    let service: UsersService;
+    let serviceMock = {
+      create : jest.fn((user) => user),
+    };
+    const user: CreateUserDto = {
+      email: "test@gmail.com",
+      password: "123456",
+  };
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-      providers: [UsersService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [UsersController],
+            providers: [UsersService, PrismaService],
+        })
+            .overrideProvider(UsersService)
+            .useValue(serviceMock)
+            .compile();
 
-    controller = module.get<UsersController>(UsersController);
-  });
+        controller = module.get<UsersController>(UsersController);
+        service = module.get<UsersService>(UsersService);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it("should be defined", () => {
+        expect(controller).toBeDefined();
+    });
+
+    describe("Users", () => {
+        describe("POST / - create user", () => {
+            it("should be defined", () => {
+                expect(controller.create).toBeDefined();
+            });
+            it('should call service.create', async () => {
+              await controller.create(user);
+              expect(service.create).toHaveBeenCalled();
+            });
+            it ('should return UserCreated', async () => {
+              const response = await controller.create(user);
+              expect(response).toBe(user);
+            })
+        });
+    });
 });
